499d3119e2aef32bd8240768df085bee2f5c5db6
Emil Lindström <lindstromemil86@gmail.com>
2021-08-08 16:51:29 +0200

    fixed buttons to acually call the game function.

--- game.js
+++ game.js
@@ -2,106 +2,129 @@ const rockButton = document.querySelector('.btn-rock');
 const paperButton = document.querySelector('.btn-paper');
 const scissorsButton = document.querySelector('.btn-scissors');
 
-
-
-rockButton.addEventListener('click', () => {
+rockButton.addEventListener('click', ()  => {
   const playerChoise = 'rock';
   console.log(playerChoise);
-  console.log(computerPlayer());
+  gameRound(playerChoise, computerPlayer());
+  
 })
-
 paperButton.addEventListener('click', () => {
    const playerChoise = 'paper';
   console.log(playerChoise);
-  console.log(computerPlayer());
+  
+  gameRound(playerChoise, computerPlayer());
 
- 
 })
 scissorsButton.addEventListener('click', () => {
   const playerChoise = 'scissors';
   console.log(playerChoise);
-  console.log(computerPlayer());
+  gameRound(playerChoise, computerPlayer());
 
-  
 })
-
 
 let playerScore = 0;
 let computerScore = 0;
 
+function gameRound(playerChoise) {
+  
+  if(playerChoise === computerPlayer()){
+console.log("Draw!");
+} else if(playerChoise === "rock" && computerPlayer() === "paper"){
+  computerScore++;
+console.log("Computer beats you!");
+} else if(playerChoise === "rock" && computerPlayer() === "scissors"){
+  playerScore++;
+console.log("You win! Rock beats Scissors");
+} else if(playerChoise === "paper" && computerPlayer() === "scissors"){
+  computerScore++;
+console.log("Computer beats you, Scissors beat Paper!")
+} else if (playerChoise === "paper" && computerPlayer() === "rock"){
+  playerScore++;
+console.log("You win, Paper beats Rock!");
+} else if (playerChoise === "scissors" && computerPlayer() === "rock"){
+  computerScore++;
+console.log("Computer wins, Rock beat Scissors");
+} else if (playerChoise === "rock" && computerPlayer() === "paper"){
+  computerScore++;
+console.log("computer wins, Paper beats Rock!");
+} else if (playerChoise === "scissors" && computerPlayer() === "paper"){
+  playerScore++;
+console.log("You win! Scissors beat paper!");
+}
+} 
 // Function that randomize computer RPC
 
 function computerPlayer(){
+    
     const randomPlay = (Math.floor(Math.random() * 3))
   
     if (randomPlay === 0){
+      let computerChoise = "paper";
     return 'rock';
   }
   else if (randomPlay === 1){
+    let computerChoise = "paper"
     return 'paper';
   }
   else if (randomPlay=== 2){
-  return 'scissors';
-  }
-    }
-
-
-
-/*
-// For loop 5 times over the code. Probably refactor later. 
-
-//for (let i = 0; i < 5; i++) {
-
-//let playerChoise = prompt('--ROCK, PAPER, SCISSORS!-- \n\nYou are playing the computer in best of 5:\n\n Now choose your weapon (Rock, Paper or Scissors): ').toLowerCase();
-const computerChoise = computerPlayer();
-console.log(gameRound(playerChoise, computerChoise));
-console.log(`Your Current Score: ${playerScore}`);
-console.log(`Computer Score: ${computerScore}`);
-
-  function gameRound(playerChoise, computerChoise) {
-      
-    
-      if(playerChoise === computerChoise){
-    return ("Draw!");
-  } else if(playerChoise === "rock" && computerChoise === "paper"){
-      computerScore++;
-    return("Computer beats you!");
-  } else if(playerChoise === "rock" && computerChoise === "scissors"){
-      playerScore++;
-    return("You win! Rock beats Scissors");
-  } else if(playerChoise === "paper" && computerChoise === "scissors"){
-      computerScore++;
-    return("Computer beats you, Scissors beat Paper!")
-  } else if (playerChoise === "paper" && computerChoise === "rock"){
-      playerScore++;
-    return("You win, Paper beats Rock!");
-  } else if (playerChoise === "scissors" && computerChoise === "rock"){
-      computerScore++;
-    return("Computer wins, Rock beat Scissors");
-  } else if (playerChoise === "rock" && computerChoise === "paper"){
-      computerScore++;
-    return("computer wins, Paper beats Rock!");
-  } else if (playerChoise === "scissors" && computerChoise === "paper"){
-      playerScore++;
-    return("You win! Scissors beat paper!");
-  } 
-}
-
-// Logging the winner with If statement after the above loop has run.
-
-console.log(isWinner());
-
-function isWinner() {
-  if (playerScore === computerScore) {
-    return 'THE FIVE GAMES ENDED IN A DRAW!';
-  }
-
-  else if (playerScore < computerScore) {
-    return 'COMPUTER WINS THE 5!'
-  }
-  else {
-    return 'CONGRATULATIONS YOU WON THE BEST OF 5!'
+    const computerChoise = "paper"
+  return computerChoise;
+  
   }
   
-} 
-*/
+    }
+
+// // For loop 5 times over the code. Probably refactor later. 
+
+// //for (let i = 0; i < 5; i++) {
+
+// //let playerChoise = prompt('--ROCK, PAPER, SCISSORS!-- \n\nYou are playing the computer in best of 5:\n\n Now choose your weapon (Rock, Paper or Scissors): ').toLowerCase();
+// const computerChoise = computerPlayer();
+// console.log(gameRound(playerChoise, computerChoise));
+// console.log(`Your Current Score: ${playerScore}`);
+// console.log(`Computer Score: ${computerScore}`);
+
+//   function gameRound(playerChoise, computerChoise) {
+      
+    
+//       if(playerChoise === computerChoise){
+//     return ("Draw!");
+//   } else if(playerChoise === "rock" && computerChoise === "paper"){
+//       computerScore++;
+//     return("Computer beats you!");
+//   } else if(playerChoise === "rock" && computerChoise === "scissors"){
+//       playerScore++;
+//     return("You win! Rock beats Scissors");
+//   } else if(playerChoise === "paper" && computerChoise === "scissors"){
+//       computerScore++;
+//     return("Computer beats you, Scissors beat Paper!")
+//   } else if (playerChoise === "paper" && computerChoise === "rock"){
+//       playerScore++;
+//     return("You win, Paper beats Rock!");
+//   } else if (playerChoise === "scissors" && computerChoise === "rock"){
+//       computerScore++;
+//     return("Computer wins, Rock beat Scissors");
+//   } else if (playerChoise === "rock" && computerChoise === "paper"){
+//       computerScore++;
+//     return("computer wins, Paper beats Rock!");
+//   } else if (playerChoise === "scissors" && computerChoise === "paper"){
+//       playerScore++;
+//     return("You win! Scissors beat paper!");
+//   } 
+// }
+
+// // Logging the winner with If statement after the above loop has run.
+
+// console.log(isWinner());
+
+// function isWinner() {
+//   if (playerScore === computerScore) {
+//     return 'THE FIVE GAMES ENDED IN A DRAW!';
+//   }
+
+//   else if (playerScore < computerScore) {
+//     return 'COMPUTER WINS THE 5!'
+//   }
+//   else {
+//     return 'CONGRATULATIONS YOU WON THE BEST OF 5!'
+//   }
